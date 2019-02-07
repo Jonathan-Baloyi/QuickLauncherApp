27244f97d3828c04ba6685e8fff674d1ebe967fc
Jonathan Baloyi <jonathan.baloyi@dariel.co.za>
2019-02-07 14:10:20 +0200

    Action for displaying an image when the item on the list view is selected

--- app/src/main/java/com/example/quicklauncherapp/SecondActivity.java
+++ app/src/main/java/com/example/quicklauncherapp/SecondActivity.java
@@ -1,8 +1,11 @@
 package com.example.quicklauncherapp;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -26,6 +29,15 @@ public class SecondActivity extends AppCompatActivity {
 
         ItemAdapter itemAdapter = new ItemAdapter(this, items, prices, descriptions);
         myListView.setAdapter(itemAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent showDetailActivity = new Intent(getApplicationContext(), DetailActivity.class);
+                showDetailActivity.putExtra("picture", position);
+                startActivity(showDetailActivity);
+            }
+        });
 
         //myListView.setAdapter(new ArrayAdapter<String>(this, R.layout.list_view_detail,items));
 
